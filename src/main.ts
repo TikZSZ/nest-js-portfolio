02f69ef9b47ff523a6fa2c5ec8b1d97b2711e759
TikZSZ <repackergame@gmail.com>
2021-10-21 10:45:37 +0530

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({origin:process.env.NODE_ENV === 'production'?'https://tikzsz-portfolio.vercel.app':"*"})
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();

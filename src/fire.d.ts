8adf14d48179ca4d2bd382815d045a22334aa854
TikZSZ <repackergame@gmail.com>
2021-10-21 10:37:23 +0530

    initial commit

--- src/fire.d.ts
+++ src/fire.d.ts
@@ -1,3 +1,5 @@
+import {DocumentData,Firestore,CollectionReference} from "@firebase/firestore/dist/lite"
+
 declare module '@firebase/firestore/dist/lite'{
   export declare function collection<T= DocumentData>(firestore: Firestore, path: string, ...pathSegments: string[]): CollectionReference<T>;
 }

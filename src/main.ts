790b95c36c5ee0b4730dc66462feb4edff55390c
TikZSZ <repackergame@gmail.com>
2021-10-25 10:20:49 +0530

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({credentials:true,origin:process.env.NODE_ENV === 'production'?'https://tikzsz-portfolio.vercel.app':"*",exposedHeaders:'set-cookie',preflightContinue:true})
+  app.enableCors({credentials:true,origin:process.env.NODE_ENV === 'production'?'https://tikzsz-portfolio.vercel.app':"*",exposedHeaders:'set-cookie',preflightContinue:true,allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe'})
   //{origin:process.env.NODE_ENV === 'production'?'https://tikzsz-portfolio.vercel.app':"*"}
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT || 3000);
